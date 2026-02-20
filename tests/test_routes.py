586642024597f5d98e13022a3ece80ca413403a5
Juan Sosa <jsosa@eit.org>
2026-02-20 06:31:28 -0500

    test read

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -168,3 +168,31 @@ class TestAccountService(TestCase):
         }
         account.deserialize(data)
         self.assertEqual(account.date_joined.isoformat(), date.today().isoformat())
+
+    def test_read_an_account(self):
+        """It should Read an existing Account"""
+        # Create new accounts and get 1
+        account = self._create_accounts(1)[0]
+
+        # Read the account with its ID
+        resp = self.client.get(
+            f"{BASE_URL}/{account.id}", 
+            content_type="application/json"
+        )
+
+        # Check status code
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+        # Assert martch
+        data = resp.get_json()
+        self.assertEqual(data["id"], account.id)
+        self.assertEqual(data["name"], account.name)
+        self.assertEqual(data["email"], account.email)
+        self.assertEqual(data["address"], account.address)
+        self.assertEqual(data["phone_number"], account.phone_number)
+        self.assertEqual(data["date_joined"], str(account.date_joined))
+
+    def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
